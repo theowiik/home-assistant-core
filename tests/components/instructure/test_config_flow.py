60cbcb10af82e843e5293c09bc761534c3eba1b7
Caodongying <jsjhcdy@163.com>
2023-12-09 11:49:57 +0100

    Done with testing config_flow. Combination of unit test and integration test

--- tests/components/instructure/test_config_flow.py
+++ tests/components/instructure/test_config_flow.py
@@ -4,7 +4,7 @@ from unittest.mock import AsyncMock, patch
 import pytest
 
 from homeassistant import config_entries
-from homeassistant.components.instructure.config_flow import CannotConnect, InvalidAuth
+from homeassistant.components.instructure.config_flow import get_courses_names
 from homeassistant.components.instructure.const import DOMAIN, HOST_PREFIX, ACCESS_TOKEN, CONF_COURSES
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
@@ -85,80 +85,94 @@ async def test_sucessful_flow(
         67890: "Test Course 2",
     }
 
+async def test_flow_with_cannot_connect_failure(
+    hass: HomeAssistant,
+    httpx_mock: HTTPXMock
+) -> None:
+    """Test flow with connection failure."""
+    httpx_mock.add_response(
+        method="GET",
+        url=f"https://{TEST_HOST_PREFIX}.instructure.com/api/v1/courses?",
+        headers={"Authorization": f"Bearer {TEST_ACCESS_TOKEN}"},
+        status_code=401,
+        json={"test_response_key": "test_response_value"}
+    )
 
-# async def test_form(hass: HomeAssistant, mock_setup_entry: AsyncMock) -> None:
-#     """Test we get the form."""
-#     result = await hass.config_entries.flow.async_init(
-#         DOMAIN, context={"source": config_entries.SOURCE_USER}
-#     )
-#     assert result["type"] == FlowResultType.FORM
-#     assert result["errors"] is None
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN,
+        context={"source": config_entries.SOURCE_USER}
+    )
 
-#     with patch(
-#         "homeassistant.components.instructure.config_flow.PlaceholderHub.authenticate",
-#         return_value=True,
-#     ):
-#         result2 = await hass.config_entries.flow.async_configure(
-#             result["flow_id"],
-#             {
-#                 "host": "1.1.1.1",
-#                 "username": "test-username",
-#                 "password": "test-password",
-#             },
-#         )
-#         await hass.async_block_till_done()
+    assert result["step_id"] == "user"
+    assert result["type"] == "form"
 
-#     assert result2["type"] == FlowResultType.CREATE_ENTRY
-#     assert result2["title"] == "Name of the device"
-#     assert result2["data"] == {
-#         "host": "1.1.1.1",
-#         "username": "test-username",
-#         "password": "test-password",
-#     }
-#     assert len(mock_setup_entry.mock_calls) == 1
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"],
+        user_input={
+            HOST_PREFIX: TEST_HOST_PREFIX,
+            ACCESS_TOKEN: TEST_ACCESS_TOKEN,
+        }
+    )
+    assert result["step_id"] == "user"
+    assert result["type"] == "form"
 
+@pytest.mark.parametrize(
+    "mock_config_data, expected_output",
+    [
+        (
+            {
+                HOST_PREFIX: TEST_HOST_PREFIX,
+                ACCESS_TOKEN: TEST_ACCESS_TOKEN
+            },
+            {
+                "Test Course 1": 12345,
+                "Test Course 2": 67890,
+            }
+        ),
+    ]
+)
+async def test_get_courses_names_with_valid_access_token(httpx_mock: HTTPXMock, mock_config_data: dict, expected_output: dict):
+    httpx_mock.add_response(
+        method="GET",
+        url=f"https://{TEST_HOST_PREFIX}.instructure.com/api/v1/courses?per_page=50",
+        headers={"Authorization": f"Bearer {TEST_ACCESS_TOKEN}"},
+        status_code=200,
+        json=[
+            {
+            "name": "Test Course 1",
+            "id": 12345,
+            },
+            {
+            "name": "Test Course 2",
+            "id": 67890,
+            },
+        ]
+    )
 
-# async def test_form_invalid_auth(hass: HomeAssistant) -> None:
-#     """Test we handle invalid auth."""
-#     result = await hass.config_entries.flow.async_init(
-#         DOMAIN, context={"source": config_entries.SOURCE_USER}
-#     )
+    assert await get_courses_names(mock_config_data) == expected_output
 
-#     with patch(
-#         "homeassistant.components.instructure.config_flow.PlaceholderHub.authenticate",
-#         side_effect=InvalidAuth,
-#     ):
-#         result2 = await hass.config_entries.flow.async_configure(
-#             result["flow_id"],
-#             {
-#                 "host": "1.1.1.1",
-#                 "username": "test-username",
-#                 "password": "test-password",
-#             },
-#         )
+@pytest.mark.parametrize(
+    "mock_config_data, expected_output",
+    [
+        (
+            {
+                HOST_PREFIX: TEST_HOST_PREFIX,
+                ACCESS_TOKEN: TEST_ACCESS_TOKEN
+            },
+            {
+                "Test Course 1": 12345,
+                "Test Course 2": 67890,
+            }
+        ),
+    ]
+)
+async def test_get_courses_names_with_invalid_access_token(httpx_mock: HTTPXMock, mock_config_data: dict, expected_output: dict):
+    httpx_mock.add_response(
+        method="GET",
+        url=f"https://{TEST_HOST_PREFIX}.instructure.com/api/v1/courses?per_page=50",
+        headers={"Authorization": f"Bearer {TEST_ACCESS_TOKEN}"},
+        status_code=401,
+        json=[]
+    )
 
-#     assert result2["type"] == FlowResultType.FORM
-#     assert result2["errors"] == {"base": "invalid_auth"}
-
-
-# async def test_form_cannot_connect(hass: HomeAssistant) -> None:
-#     """Test we handle cannot connect error."""
-#     result = await hass.config_entries.flow.async_init(
-#         DOMAIN, context={"source": config_entries.SOURCE_USER}
-#     )
-
-#     with patch(
-#         "homeassistant.components.instructure.config_flow.PlaceholderHub.authenticate",
-#         side_effect=CannotConnect,
-#     ):
-#         result2 = await hass.config_entries.flow.async_configure(
-#             result["flow_id"],
-#             {
-#                 "host": "1.1.1.1",
-#                 "username": "test-username",
-#                 "password": "test-password",
-#             },
-#         )
-
-#     assert result2["type"] == FlowResultType.FORM
-#     assert result2["errors"] == {"base": "cannot_connect"}
+    assert await get_courses_names(mock_config_data) == {}
